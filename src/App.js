c4716f2cf2cf75335892240ce87466a6a525df8c
Asif2535 <aiqbal517@gmail.com>
2021-02-23 19:10:26 +0600

    practice useEffect and load dynamic data api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
   const nayoks = ['Razzak','Jasim','Alomgir','Salman','Sakib','Bappi','Shuvo']
@@ -18,6 +18,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>I am a React Person</p>
         <Counter></Counter>
+        <Users></Users>
         <ul>
           {
             nayoks.map(nayok => <li>{nayok}</li>)
@@ -45,12 +46,34 @@ function Counter(){
   return(
     <div>
       <h1>Count: {count}</h1>
-      <button onClick={() =>setCount(count-1)}>Decrease</button>
+      <button onMouseMove={() =>setCount(count-1)}>Decrease</button>
       <button onClick={() =>setCount(count+1)}>Increase</button>
     </div>
   )
 }
+function Users(){
+  const [users, setUsers] = useState([]);
 
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data));
+  },[])
+
+  return(
+    <div>
+      <h3>Dynamic Users: {users.length}</h3>
+      {/* {
+        console.log(users)
+      } */}
+      <ul>
+        {
+          users.map(user => <li>{user.email}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
 function Product(props){
   const productStyle = {
     border:'1px solid gray',
