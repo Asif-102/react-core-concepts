bd26ab21e04867a85098b08df651b514de1e33d2
Asif2535 <aiqbal517@gmail.com>
2021-02-23 18:10:58 +0600

    practice state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,83 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const nayoks = ['Razzak','Jasim','Alomgir','Salman','Sakib','Bappi','Shuvo']
+  const products =[
+    {name:'Photoshop',price:'$90.99'},
+    {name:'Illustrator',price:'$60.99'},
+    {name:'PDF Reader',price:'$6.99'},
+    {name:'Premier El',price:'$249.99'}
+]
+  const nayokNames = nayoks.map(nayok => nayok);
+  console.log(nayokNames);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        
+        <p>I am a React Person</p>
+        <Counter></Counter>
+        <ul>
+          {
+            nayoks.map(nayok => <li>{nayok}</li>)
+          }
+          {
+            products.map(product =><li>{product.name}</li>)
+          }
+        </ul>
+        {
+          products.map(pd =><Product product={pd}></Product>)
+        }
+        {/* <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Product product={products[2]}></Product> */}
+        <Person name="Munna" job="football"></Person>
+        <Person name="Masum" job="Dorshok"></Person>
       </header>
     </div>
   );
+}
+
+function Counter(){
+  const [count, setCount]=useState(0);
+  // const handleIncrease = () =>setCount(count+1);
+  return(
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() =>setCount(count-1)}>Decrease</button>
+      <button onClick={() =>setCount(count+1)}>Increase</button>
+    </div>
+  )
+}
+
+function Product(props){
+  const productStyle = {
+    border:'1px solid gray',
+    borderRadius:'5px',
+    backgroundColor:'lightgray',
+    height:'200px',
+    width:'200px',
+    float:'left'
+  }
+  // console.log(props);
+  const {name, price}=props.product;
+  return (
+    <div style={productStyle}>
+      <h3>{name}</h3>
+      <h5>{price}</h5>
+      <button>Buy now</button>
+    </div>
+  )
+}
+
+function Person(props){
+  return (
+    <div style={{border:'2px solid gold',width:'400px',margin:'10px'}}>
+      <h3>My Name: {props.name}</h3>
+      <p>My Profession: {props.job}</p>
+    </div>
+  )
 }
 
 export default App;
